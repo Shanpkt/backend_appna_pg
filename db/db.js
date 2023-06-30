e2ef5a73a807445e59e44d29f66ed71e8a1921aa
Shantanu <shantanupathak01@gmail.com>
2023-06-30 11:48:24 +0530

    first commit

--- db/db.js
+++ db/db.js
@@ -2,18 +2,25 @@
 
 const mongoose=require('mongoose')
 
-const uri="mongodb+srv://shaan:shaan@cluster0.cmbuxv1.mongodb.net/appna_pg?retryWrites=true&w=majority"
-                     
+const uri="mongodb+srv://vercel-admin-user:p3vxxZ4UnDJhskAx@cluster0.cmbuxv1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+                        
 const connectDB=()=>{
+
 
     return mongoose.connect(uri,{
 
+      
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    
-    }).then(()=>{console.log("yes")}).catch(()=>{console.log("no")})
+     
+
+    }).then(()=>{console.log("yes")}).catch(()=>{console.log("nooooo")})
 
     
 }
 
+
+ 
 module.exports=connectDB
